13d0fbdee1758909edca2e5e9d50539b3ca2c82f
harry <h.morris@cascoda.com>
2017-03-23 13:18:40 +0000

    Add timeout to spi completion

--- Module/ca8210.c
+++ Module/ca8210.c
@@ -1013,7 +1013,20 @@ static int ca8210_spi_exchange(
 			goto cleanup;
 		}
 
-		wait_for_completion_interruptible(&priv->spi_transfer_complete);
+		wait_remaining = wait_for_completion_interruptible_timeout(
+			&priv->spi_transfer_complete,
+			msecs_to_jiffies(1000)
+		);
+		if (wait_remaining == -ERESTARTSYS) {
+			status = -ERESTARTSYS;
+		} else if (wait_remaining == 0) {
+			dev_err(
+				&spi->dev,
+				"SPI downstream transfer timed out!\n"
+			);
+			status = -ETIME;
+			goto cleanup;
+		}
 	} while (status < 0);
 
 	if (!((buf[0] & SPI_SYN) && response))
@@ -1032,9 +1045,9 @@ static int ca8210_spi_exchange(
 		);
 		status = -ETIME;
 	}
-	priv->sync_command_response = NULL;
 
 cleanup:
+	priv->sync_command_response = NULL;
 	return status;
 }
 
